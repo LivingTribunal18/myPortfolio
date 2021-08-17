876c94874426f415d63177d1c3e02fcb235282b3
karineB <karinebondarenko03@gmail.com>
2021-08-17 13:24:44 +0500

    Add files via upload

--- telegram.php
+++ telegram.php
@@ -1,10 +1,10 @@
 <?php 
 /* https://api.telegram.org/bot1627746318:AAGfGQDBf5zTgmi_y6MPup-1FUtJTaoMKBg/getUpdates */ 
+    $name = $_POST['name']; 
+    $email = $_POST['email']; 
+    $phone = $_POST['phone']; 
+    $message = $_POST['message'];
 
-    $name = $_POST['contacts-name'];
-    $email = $_POST['contacts-email'];
-    $phone = $_POST['contacts-phone'];
-    $message = $_POST['contacts-message'];
     $token = "1627746318:AAGfGQDBf5zTgmi_y6MPup-1FUtJTaoMKBg";
     $chat_id = "-588366531";
     $arr = array(
@@ -17,14 +17,11 @@
     foreach($arr as $key => $value){
         $txt .= "<b>".$key."</b> ".$value."%0A";
     }
-
     $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
     if ($sendToTelegram) {
-        header('Location: index.php');
+        echo "Ready";
     } else {
-        echo "Error";
+        echo var_dump($arr);
     }
-
-
 ?>
